78094d6cb496793a86ac30305fd1b0cddce10dda
Erik Bauer <erik.hamburg@gmx.de>
2022-04-13 00:00:31 +0200

    building

--- apps/detection_publisher/src/detection_publisher.cpp
+++ apps/detection_publisher/src/detection_publisher.cpp
@@ -7,70 +7,66 @@
 
 #include <zmq.hpp>
 
-
 #include "Mocap_msg.h"
 #include "Mocap_msgPubSubTypes.h"
 #include "domain_participant.h"
 #include "publisher.h"
 
-#include "frame_conversions.h"
+#include "Gripper.h"
 #include "Item.h"
 #include "Quad.h"
-#include "Gripper.h"
+#include "frame_conversions.h"
 
+int main() {
 
-int main () {
+  // FastDDS objects
+  std::unique_ptr<DefaultParticipant> dp =
+      std::make_unique<DefaultParticipant>(0, "raptor");
 
-    // FastDDS objects
-    DefaultParticipant dp(0, "vision_detector");
-    DDSPublisher pub = DDSPublisher(idl_msg::Mocap_msgPubSubType(),
-                                       "mocap_vision_detection", dp.participant());
+  cpp_msg::Mocap_msg msg;
+  // pub.init();
 
-    cpp_msg::Mocap_msg msg;
-    // pub.init();
+  std::string log;
+  Item stand("Stand", dp, "mocap_srl_stand");
+  Item drop("drop", dp, "mocap_srl_drop");
+  Gripper gripper("Gripper", dp, "grip_cmd");
+  Item box("box", dp, "mocap_srl_box");
+  Quad quad("Quad", &log, dp, "mocap_srl_quad", "pos_cmd", &gripper, &stand);
+  box.getPose();
+  //  Prepare our context and socket
+  zmq::context_t context(1);
+  zmq::socket_t socket(context, ZMQ_REP);
+  socket.bind("tcp://*:5555");
 
-    std::string log; 
-    Item stand("Stand", dp, "mocap_srl_stand");
-    Item drop("drop", dp, "mocap_srl_drop");
-    Gripper gripper("Gripper", dp, "grip_cmd");
-    Item box("box", dp, "mocap_srl_box");
-    Quad quad("Quad", &log, dp, "mocap_srl_quad", "pos_cmd", &gripper, &stand);
-    quad.get_pose();
-    //  Prepare our context and socket
-    zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("tcp://*:5555");
+  while (true) {
+    zmq::message_t request;
 
-    while (true) {
-        zmq::message_t request;
+    //  Wait for next request from client
+    auto res = socket.recv(request, zmq::recv_flags::none);
+    std::cout << "Received req" << std::endl;
 
-        //  Wait for next request from client
-        auto res = socket.recv (request, zmq::recv_flags::none);
-        std::cout << "Received req" << std::endl;
-
-        // deserialize protobuf message 
-        tutorial::Detection det;
-        if (!det.ParseFromString(request.to_string())) {
-            std::cerr << "Failed to parse protobuf message." << std::endl;
-            return -1;
-        }
-
-	    std::cout << det.DebugString() << std::endl;
-	    std::cout << det.x() << std::endl;
-	    std::cout << det.y() << std::endl;
-	    std::cout << det.z() << std::endl;
-
-        msg.position.x = det.x();
-        msg.position.y = det.y();
-        msg.position.z = det.z();
-
-        // pub.publish(msg);
-
-
-        //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy ((void *) reply.data (), "World", 5);
-        socket.send (zmq::str_buffer("A"), zmq::send_flags::none);
+    // deserialize protobuf message
+    tutorial::Detection det;
+    if (!det.ParseFromString(request.to_string())) {
+      std::cerr << "Failed to parse protobuf message." << std::endl;
+      return -1;
     }
-    return 0;
+
+    std::cout << det.DebugString() << std::endl;
+    std::cout << det.x() << std::endl;
+    std::cout << det.y() << std::endl;
+    std::cout << det.z() << std::endl;
+
+    msg.position.x = det.x();
+    msg.position.y = det.y();
+    msg.position.z = det.z();
+
+    // pub.publish(msg);
+
+    //  Send reply back to client
+    zmq::message_t reply(5);
+    memcpy((void *)reply.data(), "World", 5);
+    socket.send(zmq::str_buffer("A"), zmq::send_flags::none);
+  }
+  return 0;
 }
